2d67d721fd5e120e07ef9a5969c6f9d8cf295713
Aleksei Lukin <salukitretyak@yandex.ru>
2024-04-08 14:32:32 +0400

    feat: добавил основные константы новой карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,18 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function createCardElement(cardData, deleteCallback) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardTitle = cardElement.querySelector('.card__title');
+    const cardImage = cardElement.querySelector('.card__image');
+    cardImage.src = cardData.link;
+    cardTitle.textContent = cardData.name;
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    
 
+}
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
