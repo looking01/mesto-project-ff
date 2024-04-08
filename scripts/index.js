dcd0c384bde0821cde2262511bac12b30fc67382
Aleksei Lukin <salukitretyak@yandex.ru>
2024-04-08 15:08:47 +0400

    feat: добавил колбэк в функцию создания карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const placesList = document.querySelector('.places__list')
 // @todo: Функция создания карточки
 function createCardElement(cardData, deleteCallback) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
@@ -10,9 +10,14 @@ function createCardElement(cardData, deleteCallback) {
     cardImage.src = cardData.link;
     cardTitle.textContent = cardData.name;
     const deleteButton = cardElement.querySelector('.card__delete-button');
-    
-
+    deleteButton.addEventListener('click', () => {
+        deleteCallback(cardElement);
+    });
+    return cardElement;
 }
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(item) {
+    placesList.append(createCardElement(cardData, deleteCard));
+})
