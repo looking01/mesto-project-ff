bcb6c472ad0ffbbc86582336b2421d806963de65
Aleksei Lukin <salukitretyak@yandex.ru>
2024-05-06 17:22:09 +0400

    feat: добавил слушатель событий для отправки формы редактирования профиля, добавли функцию для создания новой карточки

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ import { openPopUp, closePopUp, closeButtonPopUp, closeOverlayPopUp } from './sc
 const cardTemplate = document.querySelector('#card-template').content;
 const cardsContainer = document.querySelector('.places__list');
 const overlaysPopUp = document.querySelectorAll('.popup')
-const popupEdit = document.querySelector('.popup_type_edit');
+const popUpEdit = document.querySelector('.popup_type_edit');
+const popUpNewCard = document.querySelector('.popup_type_new-card');
 const buttonsPopUpClose = document.querySelectorAll('.popup__close');
 const buttonProfileEdit = document.querySelector('.profile__edit-button');
 const profileTitle = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
-const profileEditForm = popupEdit.querySelector('.popup__form');
+const profileEditForm = popUpEdit.querySelector('.popup__form');
 const nameInput = profileEditForm.querySelector('.popup__input_type_name');
 const jobInput = profileEditForm.querySelector('.popup__input_type_description');
-
+const buttonAddCard = document.querySelector('.profile__add-button');
+const popUpNewCardForm = popUpNewCard.querySelector('.popup__form');
+const cardInputTitle = popUpNewCard.querySelector('.popup__input_type_card-name');
+const cardLink = popUpNewCard.querySelector('.popup__input_type_url');
 
 
 function createCardElement(cardData, deleteCallback) {
@@ -28,21 +32,34 @@ function createCardElement(cardData, deleteCallback) {
         deleteCallback(cardElement);
     });
     return cardElement;
+
 }
-// @todo: Функция удаления карточки
+
+function createNewCard(evt) {
+    evt.preventDefault();
+    const cardData = {};
+
+    cardData.name = cardInputTitle.value;
+    cardData.link = cardLink.value;
+    
+    cardsContainer.prepend(createCardElement(cardData, deleteCard));
+    popUpNewCardForm.reset();
+}
+
+popUpNewCardForm.addEventListener('submit', createNewCard);
+
 function deleteCard(cardElement) {
     cardElement.remove()
 }
-// @todo: Вывести карточки на страницу
+
 initialCards.forEach(function(cardData) {
     cardsContainer.append(createCardElement(cardData, deleteCard));
 })
 
-
 buttonProfileEdit.addEventListener('click', () => {
     nameInput.value = profileTitle.textContent;
     jobInput.value = profileDescription.textContent;
-    openPopUp(popupEdit);
+    openPopUp(popUpEdit);
     
 });
 
@@ -53,3 +70,17 @@ buttonsPopUpClose.forEach((button) => {
 overlaysPopUp.forEach((overlay) => {
     overlay.addEventListener('click', closeOverlayPopUp)
 });
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileDescription.textContent = jobInput.value;
+    closePopUp(popUpEdit);
+}
+
+profileEditForm.addEventListener('submit', handleFormSubmit);
+
+buttonAddCard.addEventListener('click', () => {
+    openPopUp(popUpNewCard);
+})
+
