b4b7138ca37524ae6a2b183f971cad76dd6475e5
Aleksei Lukin <salukitretyak@yandex.ru>
2024-05-06 11:14:02 +0400

    feat: добавил глоб переменные, реализовал функцию открытия попапа редактирования профиля

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-import './pages/index.css'
+import './pages/index.css';
+import { initialCards } from './scripts/cards';
+import { openPopUp } from './scripts/modal';
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsContainer = document.querySelector('.places__list');
+const popupEdit = document.querySelector('.popup_type_edit');
+const buttonProfileEdit = document.querySelector('.profile__edit-button');
+const profileTitle = document.querySelector('.profile_title');
+const proFileDescription = document.querySelector('.profile__description');
+const 
+
+
+function createCardElement(cardData, deleteCallback) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardTitle = cardElement.querySelector('.card__title');
+    const cardImage = cardElement.querySelector('.card__image');
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => {
+        deleteCallback(cardElement);
+    });
+    return cardElement;
+}
+// @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+    cardElement.remove()
+}
+// @todo: Вывести карточки на страницу
+initialCards.forEach(function(cardData) {
+    cardsContainer.append(createCardElement(cardData, deleteCard));
+})
+
+
+buttonProfileEdit.addEventListener('click', () => {
+
+    openPopUp(popupEdit);
+} )
---
+++ src/scripts/modal.js
@@ -0,0 +1,7 @@
+function openPopUp(popUpElement) {
+    popUpElement.classList.add('popup_is-opened')
+}
+
+
+
+export {openPopUp};
