e828982f16113e03c3db8ecadedadf40fee7f3ee
Aleksei Lukin <saluki.tretyak@yandex.ru>
2024-05-25 15:50:42 +0400

    добавил функцию слушателя всем инпутам в форме и функцию слушателя всех форм

--- src/components/validation.js
+++ src/components/validation.js
@@ -3,14 +3,14 @@ const showInputError = (formElement, inputElement, errorMessage, validationConfi
   inputElement.classList.add(validationConfig.inputErrorClass);
   errorElement.textContent = errorMessage;
   errorElement.classList.add(validationConfig.errorClass);
-}
+};
 
 const hideInputError = (formElement, inputElement, validationConfig) => {
   const errorElement = formElement.querySelector(`.${inputElement.name}-error`);
   inputElement.classList.remove(validationConfig.inputErrorClass);
   errorElement.classList.remove(validationConfig.errorClass);
   errorElement.textContent = '';
-}
+};
 
 const checkInputValidity = (formElement, inputElement, validationConfig) => {
   if (inputElement.validity.patternMismatch) {
@@ -23,4 +23,23 @@ const checkInputValidity = (formElement, inputElement, validationConfig) => {
   } else {
     hideInputError(formElement, inputElement, validationConfig)
   }
-}
+};
+
+const setEventListener = (formElement, validationConfig) => {
+  const inputList = Array.from(formElement.querySelectorAll(validationConfig.inputSelector));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      checkInputValidity(formElement, inputElement, validationConfig)
+    })
+  })
+};
+
+const enableValidation = (validationConfig) => {
+  const formList = Array.from(document.querySelectorAll(validationConfig.formSelector));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function(evt) {
+      evt.preventDefault();
+    });
+    setEventListener(formElement, validationConfig);
+  });
+};
