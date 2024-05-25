80b6026806ca8669663ff91c641386a35191e5c4
Aleksei Lukin <saluki.tretyak@yandex.ru>
2024-05-25 15:27:45 +0400

    добавил функцию сокрытия ошибки инпута, функцию проверки валидности инпута

--- src/components/validation.js
+++ src/components/validation.js
@@ -5,10 +5,22 @@ const showInputError = (formElement, inputElement, errorMessage, validationConfi
   errorElement.classList.add(validationConfig.errorClass);
 }
 
-const hideInputError = () => {
-
+const hideInputError = (formElement, inputElement, validationConfig) => {
+  const errorElement = formElement.querySelector(`.${inputElement.name}-error`);
+  inputElement.classList.remove(validationConfig.inputErrorClass);
+  errorElement.classList.remove(validationConfig.errorClass);
+  errorElement.textContent = '';
 }
 
-const isValidInput =() => {
-
+const checkInputValidity = (formElement, inputElement, validationConfig) => {
+  if (inputElement.validity.patternMismatch) {
+    inputElement.setCustomValidity(inputElement.dataset.errorMessage)
+  } else {
+    inputElement.setCustomValidity('');
+  }
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage, validationConfig)
+  } else {
+    hideInputError(formElement, inputElement, validationConfig)
+  }
 }
